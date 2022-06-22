4589545207f61616ecd414630b6559f2bb001e81
AlenAvako <alen.madwatch@gmail.com>
2022-06-22 16:25:56 +0300

    Initial Commit

---
+++ TheWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheWeatherApp
+//
+//  Created by Ален Авако on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
